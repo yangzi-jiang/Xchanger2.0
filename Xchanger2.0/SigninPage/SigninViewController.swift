ed79d17cd737e6e82b50262d775dfbd92fe92bd6
Altan Tutar <altutar@davidson.edu>
2019-05-26 13:20:51 -0400

    design integrated

---
+++ Xchanger2.0/SigninPage/SigninViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SigninViewController.swift
+//  Xchanger2.0
+//
+//  Created by Altan Tutar on 5/26/19.
+//  Copyright © 2019 Altan Tutar. All rights reserved.
+//
+
+import UIKit
+
+
+// Constant for Linkedin API
+
+let clientID = "78xbpu2g4mnja9"
+let clientSecret = "aZzWyS3XEOppi7ye"
+let accessTokenEndPoint = "https://www.linkedin.com/oauth/v2/accessToken"
+let authorizationTokenEndPoint = "https://www.linkedin.com/uas/oauth2/authorization"
+
+class SigninViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
