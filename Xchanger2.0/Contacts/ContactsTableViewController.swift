2a08391aa921597cba713a481f90eb8a8c32c714
Altan Tutar <altutar@davidson.edu>
2019-06-21 15:43:53 -0400

    contacts

--- Xchanger2.0/Contacts/ContactsTableViewController.swift
+++ Xchanger2.0/Contacts/ContactsTableViewController.swift
@@ -7,107 +7,92 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
 
 class ContactsTableViewController: UITableViewController {
 
-    let items = ["Yangzi Jiang", "Jesus Ibarra", "Altan Tutar"]
+    // Hold the names of contacts in an array list
+    var myContacts: [String: AnyObject] = [:]
+    var finalNames = [String: String]()
     var currentItem = ""
+    
+    
+    // Intialize the database reference
+    var ref: DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Create the database reference whenever the view is loaded
+        ref = Database.database().reference()
+        let contactsReference = ref.child("exchanges").child(Auth.auth().currentUser!.uid)
+        let nameReference = ref.child("full_names")
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        // Download the user's contacts
+        var refHandle = contactsReference.observe(DataEventType.value, with: { (snapshot) in
+            let postDict = snapshot.value as? [String : AnyObject] ?? [:]
+            self.myContacts = postDict
+            print(self.myContacts.keys.count)
+            if (self.myContacts.keys.count != 0){
+                // Call the completion handler
+                print("Here")
+                self.getUserNames(reference: nameReference)
+            }
+        })
+        
+//        let nameReference = ref.child("full_names")
+//        for userID in self.myContacts.keys {
+//            refHandle = nameReference.child(userID).observe(DataEventType.value, with: { (snapshot) in
+//                print(snapshot.value)
+//            })
+//        }
+//
+    }
+    
+    func getUserNames(reference: DatabaseReference){
+        print("From user names!")
+        let nameReference = ref.child("full_names")
+        
+       
+            var refHandle = nameReference.observe(DataEventType.value, with: { (snapshot) in
+                let postDict = snapshot.value as? [String : AnyObject] ?? [:]
+                
+                for userID in self.myContacts.keys {
+                self.finalNames[postDict[userID] as! String] = userID
+                }
+                
+                self.tableView.reloadData()
+            })
     }
 
-    // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 0
-//    }
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return finalNames.keys.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        cell.textLabel?.text = items[indexPath.row]
-        
+        let stringArray = Array(self.finalNames.keys.map { String($0) })
+
+        cell.textLabel?.text = stringArray[indexPath.row]
         return cell
     }
-    
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        currentItem = items[indexPath.row]
+        let stringArray = Array(self.finalNames.keys.map { String($0) })
+
+        currentItem = self.finalNames[stringArray[indexPath.row]]!
+        
+        print(currentItem)
         performSegue(withIdentifier: "showDetail", sender: nil)
     }
-    
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let viewController = segue.destination as? ContactsViewController {
             viewController.text = currentItem
