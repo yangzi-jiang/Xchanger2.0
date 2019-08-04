8f138c1dfc7c0f78d564837b4409a32de6970809
Yangzi Jiang <yzj999@gmail.com>
2019-08-04 14:01:06 -0400

    Working on ContactTableViewController

--- Xchanger2.0/Contacts/ContactsTableViewController.swift
+++ Xchanger2.0/Contacts/ContactsTableViewController.swift
@@ -11,13 +11,14 @@ import Firebase
 import FirebaseDatabase
 import FirebaseAuth
 
-class ContactsTableViewController: UITableViewController {
-
+class ContactsViewController:  UIViewController, UITableViewDataSource, UISearchBarDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     // Hold the names of contacts in an array list
     var myContacts: [String: AnyObject] = [:]
     var finalNames = [String: String]()
     var currentItem = ""
-    
+    var filteredData: [String]!
     
     // Intialize the database reference
     var ref: DatabaseReference!
@@ -29,78 +30,82 @@ class ContactsTableViewController: UITableViewController {
         ref = Database.database().reference()
         let contactsReference = ref.child("exchanges").child(Auth.auth().currentUser!.uid)
         let nameReference = ref.child("full_names")
-
         // Download the user's contacts
         var refHandle = contactsReference.observe(DataEventType.value, with: { (snapshot) in
             let postDict = snapshot.value as? [String : AnyObject] ?? [:]
-            self.myContacts = postDict
-            print(self.myContacts.keys.count)
+//            print(self.myContacts.keys.count)
             if (self.myContacts.keys.count != 0){
                 // Call the completion handler
                 print("Here")
                 print("From user names!")
                 var refHandle2 = nameReference.observe(DataEventType.value, with: { (snapshot) in
                     let postDict = snapshot.value as? [String : AnyObject] ?? [:]
-                    //print(type(of: self.myContacts))
-//                    self.myContacts.sorted(by:{$0.key > $1.key})
-                    for userID in self.myContacts.keys {
-                        self.finalNames[postDict[userID] as! String] = userID
-                    }
+                    self.myContacts = postDict
+//                    for userID in self.myContacts.keys {
+//                        self.finalNames[postDict[userID] as! String] = userID
+//                    }
                     
                     self.tableView.reloadData()
                 })
             }
         })
-        
-//        let nameReference = ref.child("full_names")
-//        for userID in self.myContacts.keys {
-//            refHandle = nameReference.child(userID).observe(DataEventType.value, with: { (snapshot) in
-//                print(snapshot.value)
-//            })
-//        }
-//
+//        tableView.dataSource = self
+//        searchBar.delegate = self
+        filteredData = Array(myContacts.keys)
     }
-    
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return finalNames.keys.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        let stringArray = Array(self.finalNames.keys.map { String($0) })
-
-        cell.textLabel?.text = stringArray[indexPath.row]
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = filteredData[indexPath.row]
         return cell
     }
-
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let stringArray = Array(self.finalNames.keys.map { String($0) })
-
-        currentItem = self.finalNames[stringArray[indexPath.row]]!
-        
-        performSegue(withIdentifier: "showDetail", sender: nil)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredData.count
     }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let destinationViewController = segue.destination as? ContactViewController {
-            
-            destinationViewController.userID = currentItem
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        // When user has entered text into the search box
+        // Use the filter method to iterate over all items in the data array
+        // For each item, return true if the item should be included and false if the
+        // item should NOT be included
+        filteredData = searchText.isEmpty ? Array(myContacts.keys) : Array(myContacts.keys).filter { (item: String) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
         }
-    }
-    
-    // Lock Portrait Orientation
-    override func viewWillAppear(_ animated: Bool) {
-        AppDelegate.AppUtility.lockOrientation(UIInterfaceOrientationMask.portrait, andRotateTo: UIInterfaceOrientation.portrait)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        AppDelegate.AppUtility.lockOrientation(UIInterfaceOrientationMask.all)
         
+        tableView.reloadData()
     }
+//
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//
+//        let stringArray = Array(self.finalNames.keys.map { String($0) })
+//
+//        cell.textLabel?.text = stringArray[indexPath.row]
+//        return cell
+//    }
+//
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let stringArray = Array(self.finalNames.keys.map { String($0) })
+//
+//        currentItem = self.finalNames[stringArray[indexPath.row]]!
+//
+//        performSegue(withIdentifier: "showDetail", sender: nil)
+//    }
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if let destinationViewController = segue.destination as? ContactViewController {
+//
+//            destinationViewController.userID = currentItem
+//        }
+//    }
+//
+//    // Lock Portrait Orientation
+//    override func viewWillAppear(_ animated: Bool) {
+//        AppDelegate.AppUtility.lockOrientation(UIInterfaceOrientationMask.portrait, andRotateTo: UIInterfaceOrientation.portrait)
+//    }
+//
+//    override func viewWillDisappear(_ animated: Bool) {
+//        AppDelegate.AppUtility.lockOrientation(UIInterfaceOrientationMask.all)
+//
+//    }
 }
--- Xchanger2.0/Contacts/ContactsViewController.swift
+++ Xchanger2.0/Contacts/ContactsViewController.swift
@@ -6,33 +6,33 @@
 //  Copyright © 2019 Altan Tutar. All rights reserved.
 //
 
-import UIKit
-
-class ContactsViewController: UIViewController {
-
-//    @IBOutlet weak var textView: UITextView!
-    
-    // Lock Portrait Orientation
-
-    var text = ""
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        AppDelegate.AppUtility.lockOrientation(UIInterfaceOrientationMask.portrait, andRotateTo: UIInterfaceOrientation.portrait)
-//        textView.text = text
-    }
-    
-    @IBAction func goBack(_ sender: Any) {
-        navigationController?.popViewController(animated: false)
-    }
-    
-    // Lock Portrait Orientation
-    override func viewWillDisappear(_ animated: Bool) {
-        AppDelegate.AppUtility.lockOrientation(UIInterfaceOrientationMask.all)
-        
-    }
-    
-}
+//import UIKit
+//
+//class ContactsViewController: UIViewController {
+//
+////    @IBOutlet weak var textView: UITextView!
+//    
+//    // Lock Portrait Orientation
+//
+//    var text = ""
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+//    
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        AppDelegate.AppUtility.lockOrientation(UIInterfaceOrientationMask.portrait, andRotateTo: UIInterfaceOrientation.portrait)
+////        textView.text = text
+//    }
+//    
+//    @IBAction func goBack(_ sender: Any) {
+//        navigationController?.popViewController(animated: false)
+//    }
+//    
+//    // Lock Portrait Orientation
+//    override func viewWillDisappear(_ animated: Bool) {
+//        AppDelegate.AppUtility.lockOrientation(UIInterfaceOrientationMask.all)
+//        
+//    }
+//    
+//}
